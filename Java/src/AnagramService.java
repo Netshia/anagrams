98156e9006306b39c9ae0f33b06a4577d4728255
Netshia <50585751+Netshia@users.noreply.github.com>
2020-08-21 10:33:25 +0200

    Update AnagramService.java

--- Java/src/AnagramService.java
+++ Java/src/AnagramService.java
@@ -15,42 +15,60 @@ public class AnagramService {
         List<String> words = FileUtils.readLines(new File(dictionaryLocation));
         
         List<AnagramCounter> makeAnagram = new ArrayList<>();
-        List<Integer> wordLength = new ArrayList<>(); //Storig the langth of charactors
-        List<Integer> lengthofanagrams = new ArrayList<>(); //Storing the the lemgth(count) of anagram per charactor
+        
+        //Storig the langth of charactors
+        List<Integer> wordLength = new ArrayList<>(); 
+        
+        //Storing the the lemgth(count) of anagram per charactor
+        List<Integer> lengthofanagrams = new ArrayList<>();
          
 
             for (String word : words) {
-
+                
+                //Adds the sum of charactor of word
                 wordLength.add(word.length());
                 
                 //Calls from permutation function below.
                 ArrayList<String> permutations = permute(word);
                 List<String> anagrams = new ArrayList<String>();
-                
+                    
+                    //Loop through all the permtation,
+                    //and adds all permutations in a list
                     for (String x : permutations) {
                         
                             if (words.contains(x) && !word.equals(x)) {
                                     anagrams.add(x);
                             }
                     }
-                    //System.out.println(anagrams.size());
+                   
+                    //Adding the number of anagrams found
+                    //in the file for each word to a list
                     lengthofanagrams.add(anagrams.size());   
 
             }   
 
                 for(int i: wordLength){
 
+                    //create an instance of a class(AnagramCounter)
+                    //per each word
                     makeAnagram.add(new AnagramCounter(i, lengthofanagrams.size()));
                 }
-
+            
+            //returns each instance of class (AnagramCounter)
             return makeAnagram;
     }
 
-        //Permutation function.
-        //
+        /**  *Permutation function.
+         *The function will reorder the letters,
+         *and return all possible set of permutations per word
+	 * Will create a random permutation of given string
+	 * 
+	 * @param inputword
+	 * @return permuted string
+	 */
     private static ArrayList<String> permute(String inputword) {
         ArrayList<String> permutations = new ArrayList<String>();
-                    // base case
+                   
             if (inputword.equals("")) {
                  ArrayList<String> empty = new ArrayList<String>();
                  empty.add("");
